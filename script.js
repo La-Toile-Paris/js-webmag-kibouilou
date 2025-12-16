fd305055690f103dbc10c4a3a2899250c121bb80
kibouilou <nonokibouilou@gmail.com>
2025-12-16 16:50:17 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -12,12 +12,50 @@ function getData() {
       console.log(journal);
 
       // TODO 1: REMPLIR LE HEADER
+      let header=document.getElementById("nom-journal");
+      console.log(header);
+      header.innerHTML= journal.title;
+  
+     let container=document.getElementById("phrase-accroche");
+      console.log(container);
+      container.innerHTML= journal.subtitle;
+      
 
       // TODO 2: REMPLIR LA NAVIGATION
+      let navBar=document.getElementById("themes-nav");
+      console.log(navBar);
+      navBar.innerHTML= journal.logo;
+      
+  
 
       // TODO 3: REMPLIR L'ARTICLE PRINCIPAL
+let articleprincipal=document.getElementById("article-principal")
+console.log(articleprincipal);
+
+
+let cartePrincipal= `
+<div class= 'ontainer-full'>
+<img src="${journal.feature.imageHero}" id=hero-image>
+<h3 id="hero-titre">${journal.feature.titre}"</h2>
+<h3 id=hero-info>${journal.feature.summary}</h3>
+<p id=hero-description>${journal.feature.body}</p>
+<p id=hero-auteur>${journal.feature.author}</p>
+
+</div>
+`
+articleprincipal.insertAdjacentHTML('beforeend' ,cartePrincipal);
+
 
       // TODO 4: REMPLIR LA GRILLE D'ARTICLES
+      let articleGrid=document.getElementById("articles-grid")
+      console.log(articleGrid);
+
+    journal.stories.forEach(element => {
+      let carteGril= `<div`
+      <div
+        
+      });
+      
 
       // TODO 5: REMPLIR LES THEMES
 
