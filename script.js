7b0b68652c5705cd17044cca0643d5f07c89ed20
kibouilou <nonokibouilou@gmail.com>
2025-12-17 16:52:08 +0100

    update jour 2

--- script.js
+++ script.js
@@ -22,9 +22,21 @@ function getData() {
       
 
       // TODO 2: REMPLIR LA NAVIGATION
+      let nom=journal.topics.nom;
+      console.log(nom);
+      
       let navBar=document.getElementById("themes-nav");
       console.log(navBar);
-      navBar.innerHTML= journal.logo;
+      journal.topics.forEach(function(topic){
+
+        let bouton= `<bouton class='nav-theme-btn'>
+        <p>${topic.icon}<p>
+        <h3>${topic.title}</h3>
+
+        </bouton>
+        `;
+        navBar.insertAdjacentHTML("beforeend", bouton);
+      });
       
   
 
@@ -33,35 +45,95 @@ let articleprincipal=document.getElementById("article-principal")
 console.log(articleprincipal);
 
 
-let cartePrincipal= `
-<div class= 'ontainer-full'>
-<img src="${journal.feature.imageHero}" id=hero-image>
-<h3 id="hero-titre">${journal.feature.titre}"</h2>
-<h3 id=hero-info>${journal.feature.summary}</h3>
-<p id=hero-description>${journal.feature.body}</p>
-<p id=hero-auteur>${journal.feature.author}</p>
 
+let cartePrincipal= `
+
+<img src="${journal.feature.imageHero}" id=hero-image>
+<div class="article-principal">
+  <h3 id="hero-titre">${journal.feature.titre}"</h3>
+  <h3 id=hero-info>${journal.feature.summary}</h3>
+  <p id=hero-description><strong>${journal.feature.body}<strong></p>
+  <p id=hero-auteur>${journal.feature.author}</p>
+  <p>${journal.feature.date}</p>
 </div>
-`
-articleprincipal.insertAdjacentHTML('beforeend' ,cartePrincipal);
+`;
+articleprincipal.insertAdjacentHTML('beforeend', cartePrincipal);
 
 
       // TODO 4: REMPLIR LA GRILLE D'ARTICLES
       let articleGrid=document.getElementById("articles-grid")
       console.log(articleGrid);
-
-    journal.stories.forEach(element => {
-      let carteGril= `<div`
-      <div
-        
-      });
+     
       
+    journal.stories.forEach(function(story) {
+      let carteStories= 
+      `<article class='card'>
+      <img src="${story.image}" alt=${story.headline}">
+      <button class="theme-badge"> ${story.theme}</button>
 
+      <h3>${story.headline}</h3>
+      <h3>${story.body}</h3>
+      <p>${story.summary}</p>
+      <p>${story.author}</p>
+      <p>${story.date}</p>
+      <button class="read-btn">Lire l'article</button>
+
+      </article>
+      `;
+  articleGrid.insertAdjacentHTML("beforeend", carteStories);
+      });
+   
       // TODO 5: REMPLIR LES THEMES
+      
+let containerThemes=document.getElementById('themes-list');
+console.log(containerThemes);
+
+journal.topics.forEach(function(topic){
+  carteThemes= `<div class='theme-item'>
+   <h2>${topic.icon}</h2>
+   <h3>${topic.title}</h3>
+   <p>${topic.description}</p>
+ 
+  </div>`;
+containerThemes.insertAdjacentHTML("beforeend", carteThemes);
+});
 
       // TODO 6: REMPLIR LES AUTEURS
+let auteur=document.getElementById("authors-list");
+console.log(auteur);
+
+journal.contributors.forEach(function (author) {
+  carteAuteur= `<div class="authors-card">
+  <div class='container'>
+  <img src="${author.image}">
+  <h3>${author.firstName}</h3>
+  <h3> ${author.presentation}</h3>
+  <p>E-mail: ${author.email}</p>
+  <p>${author.typeExperience}</p>
+  <p> Followers : ${author.followers}</p>
+  <p>${author.articles} Artcle</p>
+  
+  </div>`;
+  auteur.insertAdjacentHTML("beforeend" , carteAuteur);
+});
 
       // TODO 7: REMPLIR LE BOUTON CALL TO ACTION
+let cta=document.getElementById("call-to-action");
+console.log(cta);
+cta.textContent="S'habiller";
+cta.addEventListener("click" , function(){
+   alert("vous êtes abonné!");
+
+     });
+
+  
+
+
+
+
+
+
+
 
 
       /// FIN DU CODE
